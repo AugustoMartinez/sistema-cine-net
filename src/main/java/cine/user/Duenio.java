05f36989e4290d9be24a393fb787eb35004ecb93
Augusto Martinez <augustomartinezt5@gmail.com>
2023-06-17 18:43:29 -0300

    Arreglo commit

--- src/main/java/cine/user/Duenio.java
+++ src/main/java/cine/user/Duenio.java
@@ -1,10 +1,6 @@
 package cine.user;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Duenio extends Usuario{
-    List<Cliente> user = new ArrayList();
     public Duenio(String email, String password, String nombre, String apellido) {
         super(email, password, nombre, apellido);
     }
