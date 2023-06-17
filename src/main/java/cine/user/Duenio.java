c26677ff39b55b71a07b8db738e812a51c0cf6b4
Augusto Martinez <augustomartinezt5@gmail.com>
2023-06-17 18:42:32 -0300

    Commit prueba

--- src/main/java/cine/user/Duenio.java
+++ src/main/java/cine/user/Duenio.java
@@ -1,6 +1,10 @@
 package cine.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Duenio extends Usuario{
+    List<Cliente> user = new ArrayList();
     public Duenio(String email, String password, String nombre, String apellido) {
         super(email, password, nombre, apellido);
     }
